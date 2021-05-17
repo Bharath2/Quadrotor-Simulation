1b6aad17ac7cc0b1649f50e2c571b3b58aec7026
Bharath <iambharathchandra@gmail.com>
2021-05-17 23:39:57 +0530

    runsim

--- runsim.py
+++ runsim.py
@@ -6,48 +6,53 @@ from PathPlanning import RRTStar, Map
 from TrajGen import trajGenerator, Helix_waypoints, Circle_waypoints
 from Quadrotor import QuadSim
 import controller
+np.random.seed(8)
 
-# create map with obstacles
-obstacles = [[-5,25,0,20,35,60],
-             [30,25,0,55,35,100],
-             [45,35,0,55,60,60],
-             [45,75,0,55,85,100],
-             [-5,65,0,30,70,100],
-             [70,50,0,80,80,100]]
+# 3D boxes   lx, ly, lz, hx, hy, hz
+obstacles = [[-5, 25, 0, 20, 35, 60],
+             [30, 25, 0, 55, 35, 100],
+             [45, 35, 0, 55, 60, 60],
+             [45, 75, 0, 55, 85, 100],
+             [-5, 65, 0, 30, 70, 100],
+             [70, 50, 0, 80, 80, 100]]
 
+# limits on map dimensions
 bounds = np.array([0,100])
-mapobs = Map(obstacles,bounds,dim = 3)
+# create map with obstacles
+mapobs = Map(obstacles, bounds, dim = 3)
 
-#plan the path
-np.random.seed(7)
-rrt = RRTStar(start=np.array([80,20,10]),
-          goal=np.array((20,80,80)),
-          Map=mapobs,max_iter=500,
-          goal_sample_rate = 0.1)
+#plan a path from start to goal
+start = np.array([80,20,10])
+goal = np.array([30,80,80])
+
+rrt = RRTStar(start = start, goal = goal,
+              Map = mapobs, max_iter = 500,
+              goal_sample_rate = 0.1)
 
 waypoints, min_cost = rrt.plan()
 
-#plot the map and the path
-rrt.draw_scene(waypoints)
 
 #scale the waypoints to real dimensions
-scale = 0.2
-waypoints = scale*waypoints
+waypoints = 0.02*waypoints
 
 #Generate trajectory through waypoints
-traj = trajGenerator(waypoints,max_vel = 10,gamma = 1000000)
+traj = trajGenerator(waypoints, max_vel = 10, gamma = 1000000)
 
 #initialise simulation with given controller and trajectory
+Tmax = traj.TS[-1]
 des_state = traj.get_des_state
-sim = QuadSim(controller,des_state,Tmax = traj.TS[-1])
+sim = QuadSim(controller,des_state,Tmax)
 
-#plot the waypoints and obstacles
+#create a figure
 fig = plt.figure()
 ax = Axes3D.Axes3D(fig)
 ax.set_xlim((0,2))
 ax.set_ylim((0,2))
 ax.set_zlim((0,2))
-rrt.draw_path(ax,waypoints)
-mapobs.plotobs(ax,scale=scale)
+
+#plot the waypoints and obstacles
+rrt.draw_path(ax, waypoints)
+mapobs.plotobs(ax, scale = 0.02)
+
 #run simulation
 sim.run(ax)
